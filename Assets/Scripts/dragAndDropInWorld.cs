711f040d50141f9e776ef3f95b5b0b33aa795749
Erica Layton <3rica.layton@gmail.com>
2016-11-21 13:27:27 -0800

    add Unity project

---
+++ Assets/Scripts/dragAndDropInWorld.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dragAndDropInWorld : MonoBehaviour {
+	private bool dragging = false;
+
+	void OnMouseDown(){
+		dragging = true;
+	}
+
+	void OnMouseUp(){
+		dragging = false;
+	}
+	
+	void Update(){
+		Plane plane = new Plane (Vector3.up, new Vector3 (0, 30, 0));
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		float distance;
+		if (dragging){
+
+			if (plane.Raycast (ray, out distance)) {
+				transform.position = ray.GetPoint (distance);
+			}
+		}
+	}
+}
